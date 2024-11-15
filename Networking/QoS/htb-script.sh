e929482581721cc7d1d6469f3a8329877f6b8d2b
Emmanuel Lochin <1808832+elochin@users.noreply.github.com>
2024-11-15 11:23:51 +0100

    Update htb-script.sh

--- Networking/QoS/htb-script.sh
+++ Networking/QoS/htb-script.sh
@@ -14,14 +14,14 @@ tc qdisc add dev $DEV parent 1:0 handle 2:0 htb default 1
 tc class add dev $DEV parent 2:0 classid 2:1 htb rate 10Mbit ceil 10Mbit
 
 # Création des classe HTB à différents taux
-tc class add dev $DEV parent 2:1 classid 2:2 htb rate 10Mbit ceil 10Mbit
-tc class add dev $DEV parent 2:1 classid 2:3 htb rate 10Mbit ceil 10Mbit
-tc class add dev $DEV parent 2:1 classid 2:4 htb rate 10Mbit ceil 10Mbit
+tc class add dev $DEV parent 2:1 classid 2:2 htb rate 6Mbit ceil 10Mbit
+tc class add dev $DEV parent 2:1 classid 2:3 htb rate 3Mbit ceil 10Mbit
+tc class add dev $DEV parent 2:1 classid 2:4 htb rate 1Mbit ceil 10Mbit
 
 # Création des filtres
 # Filtre sur DPORT 5001 FLOWID 2:2
 tc filter add dev $DEV parent 2:0 protocol ip prio 1 u32 match ip dport 5001 0xffff flowid 2:2
 # Filtre sur DPORT 5002 FLOWID 2:3
-tc filter add dev $DEV parent 2:0 protocol ip prio 1 u32 match ip dport 5002 0xffff flowid 2:3
+tc filter add dev $DEV parent 2:0 protocol ip prio 2 u32 match ip dport 5002 0xffff flowid 2:3
 # Filtre sur DPORT 5003 FLOWID 2:4
-tc filter add dev $DEV parent 2:0 protocol ip prio 1 u32 match ip dport 5003 0xffff flowid 2:4
+tc filter add dev $DEV parent 2:0 protocol ip prio 3 u32 match ip dport 5003 0xffff flowid 2:4
